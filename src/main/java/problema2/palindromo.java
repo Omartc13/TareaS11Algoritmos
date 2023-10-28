2fecee01407735091a1897c8fa470a8dd72020f2
Omartc13 <118316814+Omartc13@users.noreply.github.com>
2023-10-28 01:24:28 -0500

    Problema 2 terminado

---
+++ src/main/java/problema2/palindromo.java
@@ -0,0 +1,43 @@
+package problema2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Omar Tc
+ */
+public class palindromo {
+
+    //atributos
+    //constructor
+    public static void main(String[] args) {
+
+    }
+
+    public boolean compro(String cad) {
+        cad=cad.replaceAll("\\s", "");
+        if (cad.length()<=1) {
+            return true;
+        }
+        
+        if (cad.charAt(0)!=cad.charAt(cad.length()-1)) {
+            return false;
+        }
+        
+        return compro(cad.substring(1,cad.length()-1));
+    }
+
+    public String vuel(String cadena) {
+        char[] letras = new char[cadena.length()];
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char le = cadena.charAt(i);
+            letras[i] = le;
+        }
+        for (int j = letras.length - 1; j >= 0; j--) {
+            resultado.append(letras[j]);
+        }
+        return cadena = resultado.toString();
+    }
+}
